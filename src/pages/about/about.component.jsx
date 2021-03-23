3632fb27e3e3008d43b980f75c0ff9e975e20b5c
ritik723 <ritiksingh7239@gmail.com>
2021-03-23 22:24:55 +0530

    first commit

---
+++ src/pages/about/about.component.jsx
@@ -0,0 +1,73 @@
+import React from "react";
+import Container from "react-bootstrap/Container";
+import Row from "react-bootstrap/Row";
+import Col from "react-bootstrap/Col";
+import "./about.styles.css";
+import Profile from "../../assets/img/profile/profile.webp";
+import Image from "react-bootstrap/Image";
+import Button from "react-bootstrap/Button";
+
+const About = () => {
+  return (
+    <div id="about">
+      <div className="about">
+        <h1 className="pt-3 text-center font-details pb-3">ABOUT ME</h1>
+        <Container>
+          <Row className="pt-3 pb-5 align-items-center">
+            <Col xs={12} md={6}>
+              <Row className="justify-content-center mb-2 mr-2 ">
+                <Image className="profile justify-content-end" alt="profile" src={Profile} thumbnail fluid />
+              </Row>
+            </Col>
+            <Col xs={12} md={6}>
+              <Row className=" align-items-start p-2 my-details rounded">
+                Hey there! I am <strong>&nbsp;RITIK SINGH</strong>
+                <br />A passionate programmer and a learner, born and brought up in India. I am a Front End Web Developer with React.js, Html, Css , Bootstrap and Node.js as my tech stack.
+                <br />
+                Currently i'm pursuing B.tech in computer science
+                <br />
+                Working with the clients, my goal is always driven towards providing amazing experience with the best level of quality and service to them.
+                <br />
+                {/* Along with that, I also help people as a COACH on their journey of becoming a professional programmer.  */}
+                <br />I love learning about new technologies, what problems are they solving and How can I use them to build better and scalable products.
+                <br /> <br />
+                <Col className="d-flex justify-content-center flex-wrap">
+                  <div>
+                    <a href="#contact">
+                      <Button className="m-2" variant="outline-primary">
+                        Let's talk
+                      </Button>
+                    </a>
+                  </div>
+                  <div>
+                    <a href=" https://drive.google.com/file/d/1N0x-_7Hfnw9VOKuwXObvfmUgClcv4XZj/view?usp=drivesdk" target="_blank" rel="noopener noreferrer">
+                      <Button className="m-2" variant="outline-success">
+                        My Resume
+                      </Button>
+                    </a>
+                  </div>
+                  <div>
+                    <a href="https://github.com/ritik723" target="_blank" rel="noopener noreferrer">
+                      <Button className="m-2" variant="outline-dark">
+                        GitHub
+                      </Button>
+                    </a>
+                  </div>
+                  <div>
+                    <a href="https://www.linkedin.com/in/ritik-singh-b87ab01b8" target="_blank" rel="noopener noreferrer">
+                      <Button className="m-2" variant="outline-info">
+                        LinkedIn
+                      </Button>
+                    </a>
+                  </div>
+                </Col>
+              </Row>
+            </Col>
+          </Row>
+        </Container>
+      </div>
+    </div>
+  );
+};
+
+export default About;
